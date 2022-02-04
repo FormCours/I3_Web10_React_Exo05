d952debfb289c81d49aa19d5171f1ef2f333603f
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-04 11:41:31 +0100

    Component Task-form finish

--- src/containers/todo-app/todo-app.jsx
+++ src/containers/todo-app/todo-app.jsx
@@ -1,10 +1,21 @@
+import { nanoid } from 'nanoid';
+import TaskForm from '../../components/task-form/task-form';
 
 const TodoApp = () => {
 
+    const handleNewTask = (data) => {
+        const newTask = { 
+            ...data,
+            id: nanoid(),
+            isFinish: false
+        };
+
+        console.log(newTask);
+    }
 
     return (
         <main>
-            Formulaire !!!
+            <TaskForm onNewTask={handleNewTask} />
 
             Liste taches !!!  
         </main>
