8e1668f0a01bd72fb505e1de6e2fa4da1e9eaa4a
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-04 11:57:11 +0100

    Add state form task list

--- src/containers/todo-app/todo-app.jsx
+++ src/containers/todo-app/todo-app.jsx
@@ -1,25 +1,30 @@
 import { nanoid } from 'nanoid';
+import { useState } from 'react';
 import TaskForm from '../../components/task-form/task-form';
 
 const TodoApp = () => {
 
+    // Définition d'un state pour contenir la liste des taches
+    const [tasks, setTasks] = useState([]);
+
     const handleNewTask = (data) => {
-        const newTask = { 
+        const newTask = {
             ...data,
             id: nanoid(),
             isFinish: false
         };
 
-        console.log(newTask);
-    }
+        // Ajouter la nouvelle tache dans la liste des taches
+        setTasks(taskElements => [newTask, ...taskElements]);
+    };
 
     return (
         <main>
             <TaskForm onNewTask={handleNewTask} />
 
-            Liste taches !!!  
+            Liste taches !!!
         </main>
     );
-}
+};
 
 export default TodoApp;
